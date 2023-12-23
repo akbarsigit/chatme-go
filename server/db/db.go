4d63e441aab02f1e5b1e74b080832738ef6f2c81
akbarsigit <akbarsigit3@gmail.com>
2023-12-23 21:50:27 +0700

    first commit

---
+++ server/db/db.go
@@ -0,0 +1,16 @@
+package db
+
+import "database/sql"
+
+type Database struct {
+	db *sql.DB
+}
+
+func NewDatabase() (*Database, error) {
+	db, err := sql.Open("postgres", "postgresql://root:password@localhost:5433/go-chat?sslmode=disable")
+	if err != nil {
+		return nil, err
+	}
+
+	return &Database{db: db}, nil
+}
